58d784904e99160ce3b7a5071a0da0576be1b553
map220v <map220v300@gmail.com>
2021-08-16 22:38:32 +0300

    fix wrong device codename

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p GalaxyA72/GalaxyA72.dsc
 gzip -c < workspace/Build/GalaxyA72/DEBUG_GCC5/FV/GALAXYA72_UEFI.fd >uefi.img
-cat a72g.dtb >>uefi.img
+cat a72q.dtb >>uefi.img
 echo > ramdisk
-abootimg --create boot-a72g.img -k uefi.img -r ramdisk
-heimdall.exe flash --BOOT boot-a72g.img
+abootimg --create boot-a72q.img -k uefi.img -r ramdisk
+heimdall.exe flash --BOOT boot-a72q.img
